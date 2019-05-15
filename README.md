ce8d2bf3902ce8e6be64ef0d2aff84d484c24d84
Bas-Jan de Ridder <bas-jan.de.ridder@cgi.com>
2019-05-15 10:39:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# air plugin
+
+The air plugin provides some [useful aliases](#aliases) as well as completion for
+the AIR all-in-one script (`$AIR_DEV_PATH/tools/aio.sh`).
+
+Enable it by adding `zsh-air` to the plugins array in your .zshrc file:
+
+```zsh
+plugins=(... zsh-air)
+```
+
+## Aliases
+
+| Alias                | Command                                         |
+|:---------------------|:------------------------------------------------|
+| `air`                | `$AIR_DEV_PATH/tools/aio.sh`                    |
+| `dca`                | `$AIR_DEV_PATH/tools/aio.sh`                    |
---
+++ zsh-air.plugin.zsh
@@ -0,0 +1,44 @@
+# aliases
+alias air=$AIR_DEV_PATH/tools/aio.sh
+alias dca=$AIR_DEV_PATH/tools/aio.sh
+
+function listAirCompletions {
+	reply=(
+		# options
+		-h --help
+
+		# completions
+		pokemon fetchall
+		build
+		list
+		listall
+		start
+		startall
+		restart
+		restartall
+		logs
+		logsall
+		tail logstail
+		stop
+		stopall
+		remove
+		removeall
+		cleanvolumes
+		cleanimages
+		cleanimages:force
+		greenplum
+		greenplum start
+		greenplum stop
+		greenplum delete
+		airsysteem systeem
+		airsysteem systeem start
+		airsysteem systeem stop
+		airsysteem systeem delete
+		usermanagement
+		usermanagement start
+		usermanagement stop
+		usermanagement delete
+	)
+}
+
+compctl -K listAirCompletions aio.sh
